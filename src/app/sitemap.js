80acacc63f5b5a932136a28f3d4d96265c1865fc
jahid <jahidhasanbsl46@gmail.com>
2026-05-28 02:03:55 +0600

    sitemap up

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,42 +1,24 @@
 export default function sitemap() {
-  const baseUrl = 'https://jahid-portfolio-sandy.vercel.app';
+  const baseUrl = "https://jahid-portfolio-sandy.vercel.app";
 
   return [
     {
       url: baseUrl,
       lastModified: new Date(),
-      changeFrequency: 'daily',
+      changeFrequency: "daily",
       priority: 1,
-    },
-    {
-      url: `${baseUrl}/#about`,
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 0.9,
     },
     {
       url: `${baseUrl}/skills`,
       lastModified: new Date(),
-      changeFrequency: 'weekly',
+      changeFrequency: "weekly",
       priority: 0.8,
     },
     {
       url: `${baseUrl}/projects`,
       lastModified: new Date(),
-      changeFrequency: 'weekly',
+      changeFrequency: "weekly",
       priority: 0.9,
-    },
-    {
-      url: `${baseUrl}/#education`,
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.7,
-    },
-    {
-      url: `${baseUrl}/#certifications`,
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.7,
     },
   ];
 }
