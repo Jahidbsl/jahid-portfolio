2b11a957b9daaf1cf7233c4c5b79ae9d8dc3bdd9
jahid <jahidhasanbsl46@gmail.com>
2026-05-28 02:10:24 +0600

    robots

---
+++ src/app/robots.js
@@ -0,0 +1,10 @@
+export default function robots() {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap:
+      "https://jahid-portfolio-sandy.vercel.app/sitemap.xml",
+  };
+}
